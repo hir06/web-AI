c2195485579fa0a4fbdc1ab966e07ac31dddd48f
hiral parmar <hiralparmar@hirals-MacBook-Pro.local>
2025-06-14 12:44:54 +0200

    fix: installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,38 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+// vite plugins
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import Fonts from 'unplugin-fonts/vite';
+// @ts-ignore
+import imagemin from 'unplugin-imagemin/vite';
+import { compression } from 'vite-plugin-compression2';
+import Inspect from 'vite-plugin-inspect';
+import svgr from 'vite-plugin-svgr';
+
+import { fonts } from './configs/fonts.config';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    svgr(),
+    react(),
+    Inspect(),
+    compression(),
+    imagemin(),
+    tailwindcss(),
+    Fonts({ google: { families: fonts } }),
+    AutoImport({
+      imports: ['react', 'react-router'],
+      dts: './auto-imports.d.ts',
+      eslintrc: { filepath: './eslint.config.js' },
+      dirs: ['./src/components/ui'],
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
